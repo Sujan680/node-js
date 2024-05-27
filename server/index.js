d57f432fcc99a1e20e29b17165cdb3bda2167d6e
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-05-27 08:54:01 +0000

    server creation

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req,res)=> {
+    const log = `${Date.now()} : New Req received \n`
+    fs.appendFile("log.txt", log, (err,data) => {
+       
+        switch(req.url) {
+            case"/":
+            res.end("Home Page");
+            break
+            case"/about":
+            res.end("This is about page.")
+            break
+            res.end("404 found");
+        }
+    })
+   
+});
+
+myServer.listen(8000, ()=> console.log("Server Started"));
