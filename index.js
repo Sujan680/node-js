c8787f5c3b83c75b10090e0d53eb96ec8afc8334
Sujan Budhathoki <75134082+Sujan680@users.noreply.github.com>
2024-05-27 06:41:42 +0000

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // console.log("HEllo world"); 
 
-const math = require("./math");
 
-console.log("The sum is ",math(3,4));
+// Modules in node js importing the function
+// const math = require("./math"); 
+
+// this is destructuring method
+const {Sub, Sum} = require("./math");
+
+console.log("The value is : ",Sub(3,4));
+console.log("The value of a & b is", Sum(3,9));
--- math.js
+++ math.js
@@ -1,5 +1,18 @@
+// created different functions and exported that as object 
+
 function Sum(a,b) {
     return(a+b);
 }
 
-module.exports = Sum;
+function Sub(a,b) {
+    return(a-b);
+}
+
+// here for exporting the multiple Function we used objects 
+module.exports = {
+    Sum, Sub
+};
+
+// this is the methods of exporting multiple anonymous function
+exports.Sum = (a,b) => a+b;
+exports.Sub = (a,b) => a-b;
